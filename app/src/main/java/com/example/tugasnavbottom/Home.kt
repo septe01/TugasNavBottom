5020ac8633933eac4458673d860314e816d8a6ad
vhirgo <septe.1991@gmail.com>
2019-10-17 10:48:53 +0700

    fragmen

---
+++ app/src/main/java/com/example/tugasnavbottom/Home.kt
@@ -0,0 +1,34 @@
+package com.example.tugasnavbottom
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class Home : Fragment() {
+
+    companion object {
+        fun newInstance(): Home {
+            var fragmentHome = Home()
+            var args = Bundle()
+            Home.arguments = args
+            return fragmentHome
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+}
